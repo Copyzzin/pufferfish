26b44112718ddb63750bbf7063cc03e6af418b81
Copyzzin <106496046+Copyzzin@users.noreply.github.com>
2025-08-18 22:06:03 -0300

    Create ngrok.sh

---
+++ ngrok.sh
@@ -0,0 +1,4 @@
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+tar -xf ngrok-v3-stable-linux-amd64.tgz
+./ngrok config add-authtoken 2iyaNipnGT6XH6lJ5iHmX9Z7ehZ_2Uk7h74T6TrbJA6ZsKQQk
+./ngrok http 8080
